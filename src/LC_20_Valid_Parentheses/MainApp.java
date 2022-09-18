a2cca427173b3a70ac50060f6589f436a5f3a564
ovoskresenskyy <7949894@ukr.net>
2022-09-18 21:42:16 +0300

    added solution

--- src/LC_20_Valid_Parentheses/MainApp.java
+++ src/LC_20_Valid_Parentheses/MainApp.java
@@ -1,4 +1,33 @@
 package LC_20_Valid_Parentheses;
 
+import java.util.Stack;
+
 public class MainApp {
+    public static void main(String[] args) {
+        System.out.println(Solution.isValid("([{}])"));
+    }
+}
+
+class Solution {
+    public static boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()){
+                    return false;
+                } else {
+                    char topChar = stack.peek();
+                    if (topChar == '(' && c != ')' || topChar == '[' && c != ']' || topChar == '{' && c != '}'){
+                        return false;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
 }
