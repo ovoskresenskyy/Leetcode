1e6229ae8a750f072314e109a79d9ed0f4bca8c4
ovoskresenskyy <7949894@ukr.net>
2022-09-27 11:39:41 +0300

    Added solution. Used slow and fast pointers

--- src/LC_26_Remove_Duplicates_from_Sorted_Array/MainApp.java
+++ src/LC_26_Remove_Duplicates_from_Sorted_Array/MainApp.java
@@ -6,5 +6,18 @@ public class MainApp {
 
     public static void main(String[] args) {
 
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int k = removeDuplicates(nums);
+
     }
-}
+
+    private static int removeDuplicates(int[] nums) {
+        int slowPointer = 0;
+        for (int fastPointer = 1; fastPointer < nums.length; fastPointer++) {
+            if (nums[slowPointer] != nums[fastPointer]) {
+                slowPointer++;
+                nums[slowPointer] = nums[fastPointer];
+            }
+        }
+        return slowPointer + 1;
+    }}
