c89d9139ce7ad7db76ee21e70a7be04dc856475c
ovoskresenskyy <7949894@ukr.net>
2022-09-27 11:50:05 +0300

    Added simple solution for LC 27.

--- src/LC_27_Remove_Elements/MainApp.java
+++ src/LC_27_Remove_Elements/MainApp.java
@@ -1,4 +1,22 @@
 package LC_27_Remove_Elements;
 
 public class MainApp {
+
+    public static void main(String[] args) {
+
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+
+        int k = removeElements(nums, 2);
+
+    }
+
+    private static int removeElements(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i++] = nums[j];
+            }
+        }
+        return i;
+    }
 }
