5c1afdfbe6a28a34311c34e00b67e77b748430c0
ovoskresenskyy <7949894@ukr.net>
2022-09-27 12:00:12 +0300

    Added solution for LC 35. Used binary search

--- src/LC_35_Search_Insert_Position/MainApp.java
+++ src/LC_35_Search_Insert_Position/MainApp.java
@@ -3,5 +3,29 @@ package LC_35_Search_Insert_Position;
 public class MainApp {
     public static void main(String[] args) {
 
+        int[] nums = {1, 3, 5, 6};
+        int k = searchInsert(nums, 7);
+
+        System.out.println("k = " + k);
+
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+
+            if (nums[middle] > target) {
+                right = middle - 1;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+
+        return left;
     }
 }
