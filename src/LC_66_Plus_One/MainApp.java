823517cb8f18e203c1c649f7bb67f3115e9b48ef
ovoskresenskyy <7949894@ukr.net>
2022-10-02 16:06:58 +0300

    LC 66. Added solution.
    Used cycle till [i] != 9

--- src/LC_66_Plus_One/MainApp.java
+++ src/LC_66_Plus_One/MainApp.java
@@ -1,4 +1,46 @@
 package LC_66_Plus_One;
 
+import java.util.Arrays;
+
 public class MainApp {
+
+    public static void main(String[] args) {
+        int[] digits = {1, 2, 3};
+        System.out.println(Arrays.toString(plusOne(digits)));
+
+        digits = new int[] {4, 3, 2, 1};
+        System.out.println(Arrays.toString(plusOne(digits)));
+
+        digits = new int[] {9};
+        System.out.println(Arrays.toString(plusOne(digits)));
+
+        digits = new int[] {7, 8, 9};
+        System.out.println(Arrays.toString(plusOne(digits)));
+
+        digits = new int[] {7, 8, 9, 9, 9};
+        System.out.println(Arrays.toString(plusOne(digits)));
+
+        digits = new int[] {5, 6, 9, 9};
+        System.out.println(Arrays.toString(plusOne(digits)));
+
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                break;
+            } else {
+                digits[i] = 0;
+            }
+        }
+
+        if (digits[0] == 0) {
+            int[] res = new int[digits.length + 1];
+            res[0] = 1;
+            return res;
+        }
+        return digits;
+    }
+
 }
