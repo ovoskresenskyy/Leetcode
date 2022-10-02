12795721ed2e633abf17dfa61eaec5023a1ded7a
ovoskresenskyy <7949894@ukr.net>
2022-10-02 15:47:18 +0300

    Added solution for LC 58

--- src/LC_58/MainApp.java
+++ src/LC_58/MainApp.java
@@ -1,4 +1,23 @@
 package LC_58;
 
 public class MainApp {
+
+    public static void main(String[] args) {
+
+        String s = "Hello World";
+        System.out.println(lengthOfLastWord(s));
+
+        s = "   fly me   to   the moon  ";
+        System.out.println(lengthOfLastWord(s));
+
+        s = "luffy is still joyboy";
+        System.out.println(lengthOfLastWord(s));
+
+    }
+
+    public static int lengthOfLastWord(String s) {
+        String words[] = s.trim().split(" ");
+        return words[words.length-1].length();
+    }
+
 }
