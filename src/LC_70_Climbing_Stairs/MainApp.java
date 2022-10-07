76c7e5899c8ac7f4471c3986b6ed5b445de916c2
ovoskresenskyy <7949894@ukr.net>
2022-10-07 13:17:11 +0300

    comment

--- src/LC_70_Climbing_Stairs/MainApp.java
+++ src/LC_70_Climbing_Stairs/MainApp.java
@@ -3,7 +3,17 @@ package LC_70_Climbing_Stairs;
 public class MainApp {
 
     public static void main(String[] args) {
-
+        System.out.println(climbStairs(5));
+        System.out.println(climbStairs(4));
+        System.out.println(climbStairs(10));
+        System.out.println(climbStairs(20));
     }
 
+    public static int climbStairs(int n) {
+        int a = 1, b = 1;
+        for (int i = 0; i < n; i++) {
+            a = (b += a) - a;
+        }
+        return a;
+    }
 }
