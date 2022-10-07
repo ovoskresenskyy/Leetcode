15a0697038f83884fe836eaf4f56a22b5bba4b2e
ovoskresenskyy <7949894@ukr.net>
2022-10-07 13:07:55 +0300

    Added solution for LC 69 used binary search

--- src/LC_69_SqrtX/MainApp.java
+++ src/LC_69_SqrtX/MainApp.java
@@ -3,5 +3,29 @@ package LC_69_SqrtX;
 public class MainApp {
     public static void main(String[] args) {
 
+        System.out.println(mySqrt(15));
+
+    }
+
+    public static int mySqrt(int x) {
+
+        if (x == 0) return x;
+
+        int start = 1;
+        int end = x;
+        int result = 0;
+
+        while (start <= end) {
+            //Use binary search
+            int mid = start + (end - start) / 2;
+
+            if (mid <= x / mid) {
+                result = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return result;
     }
 }
