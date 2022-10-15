5a0c029f77f4637a94cad6d3deee453383fe58c1
ovoskresenskyy <7949894@ukr.net>
2022-10-15 21:18:00 +0300

    1

---
+++ src/LC_83_Remove_Duplicates_from_Sorted_List/Main.java
@@ -0,0 +1,31 @@
+package LC_83_Remove_Duplicates_from_Sorted_List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
